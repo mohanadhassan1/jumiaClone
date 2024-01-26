1e22030361261a53b8c26ac6e22a0ef59d7b7364
elzaw <hoodaelzaw81@gamil.com>
2024-01-26 19:40:31 +0200

    error handling

--- backend/app.js
+++ backend/app.js
@@ -5,16 +5,16 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 const subcategoryRoutes = require("./routes/subcategoryRoute");
 const userRatingsRoutes = require("./routes/userRatingsRoute");
 const vendorsRoutes = require("./routes/vendorsRoute");
 const wishlistRoutes = require("./routes/wishlistRoute");
 const productRoutes = require("./routes/productRoute");
-const couponRout = require('./routes/Coupons');
-const CustomerRout = require('./routes/Customer');
-const FeaturedRout = require('./routes/FeaturedProducts');
-const OrderRout = require('./routes/Order');
-
+const couponRout = require("./routes/Coupons");
+const CustomerRout = require("./routes/Customer");
+const FeaturedRout = require("./routes/FeaturedProducts");
+const OrderRout = require("./routes/Order");
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -32,11 +32,13 @@ app.use("/subcategory", subcategoryRoutes);
 app.use("/rating", userRatingsRoutes);
 app.use("/vendor", vendorsRoutes);
 app.use("/wishlist", wishlistRoutes);
-app.use('/coupons',couponRout);
-app.use('/custmers',CustomerRout);
-app.use('/featurd',FeaturedRout);
-app.use('/order',OrderRout);
+app.use("/coupons", couponRout);
+app.use("/custmers", CustomerRout);
+app.use("/featurd", FeaturedRout);
+app.use("/order", OrderRout);
 
+app.use(notFound);
+app.use(errorHandler);
 app.listen(port, () => {
   console.log(
     `Server is running in ${process.env.NODE_ENV} mode on http://localhost:${port}`
