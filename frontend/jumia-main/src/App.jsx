f02339d26998581a9426f08ab9cdde954e624b60
mohanad <mohanadhassan246@gmail.com>
2024-02-19 11:11:40 +0200

    edit in footer

--- frontend/jumia-main/src/App.jsx
+++ frontend/jumia-main/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/index";
+import Header from "./components/Header/index";
+
 function App() {
   return (
     <>
   
       <div className="bg-orange-600 h-screen items-center justify-center">
+        <Header />
         <Navbar />
-        <div className="">main app</div>
+        <div className="h-full">main app</div>
         <Footer />
       </div>
     </>
