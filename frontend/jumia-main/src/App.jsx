f3bb3f50021ec42436cc347ce9912150508d8d8f
mohanad <mohanadhassan246@gmail.com>
2024-02-19 10:58:37 +0200

    edit app

--- frontend/jumia-main/src/App.jsx
+++ frontend/jumia-main/src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
-
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/index";
 function App() {
   return (
     <>
-      <div className="bg-orange-600 h-screen flex items-center justify-center">
+  
+      <div className="bg-orange-600 h-screen items-center justify-center">
+        <Navbar />
         <div className="">main app</div>
+        <Footer />
       </div>
     </>
   );
